e726800c77b9f72b73a5d024a56b3b782b37085e
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-10-03 21:50:16 +0530

    manufactuers dropdown done

--- app/src/main/java/com/example/autoinsight/DataCarActivity.kt
+++ app/src/main/java/com/example/autoinsight/DataCarActivity.kt
@@ -18,6 +18,28 @@ import com.example.autoinsight.DataContactActivity.Companion.m
 
 class DataCarActivity : AppCompatActivity() {
 
+    private val brands = arrayOf(
+        "Tata", "Maruti", "Mahindra", "Hyundai",
+        "Toyota", "VW", "Honda", "GM/Chevrolet",
+        "BMW", "Mercedes"
+    )
+
+    private val modelsMap = mapOf(
+        "Tata" to arrayOf("Indica", "Tiago", "Tigor", "Nexon", "Harrier","safari","Altroz","Punch","Zest","Nano","Sierra","Hexa","Aria","Sumo","Estate"),
+        "Maruti" to arrayOf("Swift", "Baleno", "Dzire", "WagonR", "Ertiga","Alto","Presso","Versa","Eeco","Brezza","Celerio","Ciaz","Ignis","S cross","XLS","Jumny","Vitara","Invicto","Cresent","Fronx"),
+        "Mahindra" to arrayOf("XUV500", "Scorpio", "Thar", "Bolero", "KUV100","XUV300","XUV700","Xylo"),
+        "Hyundai" to arrayOf("Creta", "i20", "Venue", "Verna", "Tucson","Accent", "Elentra", "Sonata", "i10", "Xcent", "Eon", "Grand i10", "Alcazar", "Aura"),
+        "Honda" to arrayOf( "Amaze", "City", "Civic", "CR-V", "Accord", "Jazz"),
+        "VW" to arrayOf("Jetta", "Polo", "Atlas", "Golf", "Touareg", "Tiguan"),
+        "Toyota" to arrayOf("Camry", "Qualis", "Innova - Crysta", "Innova", "Corolla"),
+        "GM/Chevrolet" to arrayOf("Beat", "Tavera", "Captiva", "Cruze"),
+        "BMW" to arrayOf("X1","X3","X5","X7"),
+        "Mercedes" to arrayOf("C-Class", "GLA", "S-Class", "E-Class", "A-Class", "GLE", "GLC", "GLS", "G-Class"),
+
+
+
+        )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_datacar)
@@ -28,20 +50,40 @@ class DataCarActivity : AppCompatActivity() {
         l = this.findViewById(R.id.regNo)
         m = this.findViewById(R.id.fuel)*/
 
-        val fuelType = resources.getStringArray(R.array.fuelType)
-        val arrayAdapterFuel: ArrayAdapter<String> = ArrayAdapter(this, R.layout.dropdown, fuelType)
-        val fuel = findViewById<AutoCompleteTextView>(R.id.fuel)
-        fuel.setAdapter(arrayAdapterFuel)
+        val brandAutoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.manufacturer)
+        val modelAutoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.car_model)
 
-        val carSegment = resources.getStringArray(R.array.carSegment)
-        val arrayAdapterSegment: ArrayAdapter<String> = ArrayAdapter(this, R.layout.dropdown, carSegment)
-        val segment = findViewById<AutoCompleteTextView>(R.id.segment)
-        segment.setAdapter(arrayAdapterSegment)
+        // Populate the brand AutoCompleteTextView
+        val brandAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, brands)
+        brandAutoCompleteTextView.setAdapter(brandAdapter)
+
+        // Set an item click listener for the brand AutoCompleteTextView
+        brandAutoCompleteTextView.setOnItemClickListener { _, _, position, _ ->
+            val selectedBrand = brands[position]
+            val modelsForBrand = modelsMap[selectedBrand] ?: emptyArray()
+
+            // Populate the model AutoCompleteTextView with models for the selected brand
+            val modelAdapter =
+                ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, modelsForBrand)
+            modelAutoCompleteTextView.setAdapter(modelAdapter)
 
 
-        val cnextButton = this.findViewById<Button>(R.id.cnextButton)
-        cnextButton.setOnClickListener {
-            /*if (g.text.toString().isEmpty() || h.text.toString().isEmpty() || i.text.toString().isEmpty() || l.text.toString().isEmpty() || m.text.toString().isEmpty()) {
+            val fuelType = resources.getStringArray(R.array.fuelType)
+            val arrayAdapterFuel: ArrayAdapter<String> =
+                ArrayAdapter(this, R.layout.dropdown, fuelType)
+            val fuel = findViewById<AutoCompleteTextView>(R.id.fuel)
+            fuel.setAdapter(arrayAdapterFuel)
+
+            val carSegment = resources.getStringArray(R.array.carSegment)
+            val arrayAdapterSegment: ArrayAdapter<String> =
+                ArrayAdapter(this, R.layout.dropdown, carSegment)
+            val segment = findViewById<AutoCompleteTextView>(R.id.segment)
+            segment.setAdapter(arrayAdapterSegment)
+
+
+            val cnextButton = this.findViewById<Button>(R.id.cnextButton)
+            cnextButton.setOnClickListener {
+                /*if (g.text.toString().isEmpty() || h.text.toString().isEmpty() || i.text.toString().isEmpty() || l.text.toString().isEmpty() || m.text.toString().isEmpty()) {
                 Toast.makeText(
                     applicationContext,
                     "Please fill all the mandatory * fields.",
@@ -49,17 +91,18 @@ class DataCarActivity : AppCompatActivity() {
                 )
                     .show()
             } else {*/
-            val intent = Intent(this, DataStatusActivity::class.java).apply {
+                val intent = Intent(this, DataStatusActivity::class.java).apply {
+                }
+                startActivity(intent)
+                /*}*/
             }
-            startActivity(intent)
-        /*}*/
-        }
 
-        val logout = this.findViewById<ImageView>(R.id.logout)
-        logout.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java).apply {
+            val logout = this.findViewById<ImageView>(R.id.logout)
+            logout.setOnClickListener {
+                val intent = Intent(this, LoginActivity::class.java).apply {
+                }
+                startActivity(intent)
             }
-            startActivity(intent)
         }
     }
 }
